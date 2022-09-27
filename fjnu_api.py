0c5da75ed52199b8b27c77d81430c53fa15ff6b4
MoveToEx <movetoex@outlook.com>
2022-09-27 21:40:15 +0800

    Unify interface

--- fjnu_api.py
+++ fjnu_api.py
@@ -1,52 +1,71 @@
 import requests
 import json
 import parse
+import browser_cookie3 as bc
 import re
 
+class_time = {
+    "旗山校区": [None, (8, 20), (9, 15), (10, 20), (11, 15), (14, 0), (14, 55),
+             (15, 50), (16, 45), (18, 30), (19, 25), (20, 20), (21, 15)],
+    "仓山校区": [None, (8, 00), (8, 55), (10, 00), (10, 55), (14, 0), (14, 55),
+             (15, 50), (16, 45), (18, 30), (19, 25), (20, 20), (21, 15)]
+}
+
+
 class FJNUApi:
-    def __init__(this, url_suffix, cookie, body):
-        this.url = 'https://jwglxt.fjnu.edu.cn/jwglxt/kbcx/xskbcx_cxXsgrkb.html?' + url_suffix
-        this.body = body
-        this.content = {}
-        this.headers = {
+    def requirements():
+        return ["link", "year", "semester"]
+
+    def fetch(args):
+        url_suffix = parse.parse(
+            "https://jwglxt.fjnu.edu.cn/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?{}", args['link'])[0]
+        header = {
             "Host": "jwglxt.fjnu.edu.cn",
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
             "Origin": "https://jwglxt.fjnu.edu.cn",
             "Referer": "https://jwglxt.fjnu.edu.cn/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?layout=default&" + url_suffix,
         }
-        this.cookies = cookie
+        semester = [None, 3, 12, 16][int(args['semester'])]
 
-    def fetch(this):
-        res = requests.post(this.url, headers=this.headers, data=this.body, cookies=this.cookies)
-        this.content = json.loads(res.content)['kbList']
+        res = requests.post(
+            'https://jwglxt.fjnu.edu.cn/jwglxt/kbcx/xskbcx_cxXsgrkb.html?' + url_suffix,
+            headers=header,
+            data='xnm=%s&xqm=%d&kzlx=ck' % (args['year'], semester),
+            cookies=bc.load('jwglxt.fjnu.edu.cn')
+        )
 
-    def parse(this):
+        content = json.loads(res.content)['kbList']
         res = []
-        for it in this.content:
-            _class = parse.parse("{}-{}", it['jcs'])
-            _class = list(range(int(_class[0]), int(_class[1]) + 1))
-            
+        
+        for it in content:
+            _ = parse.parse("{}-{}", it['jcs'])
+            _class = [int(_[0]), int(_[1])]
+            campus = it['xqmc']
+
             _weeks = []
             for s in it['zcd'].split(','):
                 s = s.strip()
-                if re.search("\d*-\d*周",s):
+                if re.search("\d*-\d*周", s):
                     _ = parse.parse("{}-{}周", s)
                     for i in range(int(_[0]), int(_[1]) + 1):
                         _weeks.append(i)
                 elif re.search("\d*周", s):
                     _ = parse.parse("{}周", s)
                     _weeks.append(int(_[0]))
-            
+
             # 写后端的傻逼我囸你妈
 
             res.append({
                 "name": it['kcmc'],
                 "teacher": it['xm'],
                 "location": it['cdmc'],
+                "time": {
+                    "from": (class_time[campus][_class[0]][0], class_time[campus][_class[0]][1]),
+                    "to": (class_time[campus][_class[1]][0], class_time[campus][_class[1]][1])
+                },
                 "weeks": _weeks,
                 "weekday_order": int(it['xqj']),
-                "class_order": _class,
-                "campus": it['xqmc']
+                "class_order": _class
             })
         return res
--- main.py
+++ main.py
@@ -6,32 +6,16 @@ import parse
 
 cal = ics.Calendar()
 
-class_time = {
-    "旗山校区": [None, (8, 20), (9, 15), (10, 20), (11, 15), (14, 0), (14, 55), 
-	    (15, 50), (16, 45), (18, 30), (19, 25), (20, 20), (21, 15)],
-    "仓山校区": [None, (8, 00), (8, 55), (10, 00), (10, 55), (14, 0), (14, 55), 
-	    (15, 50), (16, 45), (18, 30), (19, 25), (20, 20), (21, 15)]
-}
+duration_per_class = datetime.timedelta(minutes=45)
 
-duration_per_class = 45
+args = {}
 
-lnk = input("Link=")
-
-suffix = parse.parse("https://jwglxt.fjnu.edu.cn/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?{}", lnk)[0]
-
-yr = input("Year=")
-st = [None, 3, 12, 16][int(input("Semester="))]
-
-api = fjnu_api.FJNUApi(
-    suffix,
-    browser_cookie3.load('jwglxt.fjnu.edu.cn'),
-    'xnm=%s&xqm=%d&kzlx=ck' % (yr, st)
-)
+for key in fjnu_api.FJNUApi.requirements():
+    args[key] = input(key + '=')
 
 print("Sending POST request...")
 
-api.fetch()
-data = api.parse()
+data = fjnu_api.FJNUApi.fetch(args)
 
 print("Generating ics...")
 
@@ -42,18 +26,17 @@ for day in range(1, 366):
     if day % 7 == 1:
         week_index += 1
     for it in data:
-        campus = it['campus']
         if week_index in it['weeks'] and ((day - 1) % 7 + 1) == it['weekday_order']:
             e = ics.Event()
             e.name = it['name']
             e.begin = str(current_time + datetime.timedelta(
-                hours = class_time[campus][it['class_order'][0]][0] - 8,
-                minutes = class_time[campus][it['class_order'][0]][1]
+                hours = it['time']['from'][0] - 8,
+                minutes = it['time']['from'][1]
             ))
             e.end = str(current_time + datetime.timedelta(
-                hours = class_time[campus][it['class_order'][-1]][0] - 8,
-                minutes = class_time[campus][it['class_order'][-1]][1]
-            ) + datetime.timedelta(minutes=duration_per_class))
+                hours = it['time']['to'][0] - 8,
+                minutes = it['time']['to'][1]
+            ) + duration_per_class)
             e.location = it['location']
             e.description = '授课教师: ' + it['teacher']
             cal.events.add(e)
